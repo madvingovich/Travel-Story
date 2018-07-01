46ada164400219e40120495ea600c97c8d627aee
madvingovich <37845039+madvingovich@users.noreply.github.com>
2018-07-01 13:37:27 +0300

    js

--- js/my.js
+++ js/my.js
@@ -38,7 +38,7 @@ $(window).on({
                 }
             };
             this.horizontalShowItem = function (elem) {
-                if($(document).scrollTop() + $(window).height() > $(elem).offset().top + this.screenPercent) {
+                if($(document).scrollTop() + $(window).height() > $(elem).offset().top ) {
                     $(elem).css({"opacity":"1", "transform":"translate(0,0)"});
                 }
             };
